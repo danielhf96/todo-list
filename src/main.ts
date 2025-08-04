9299d1f48242880871322b745d09211835dd30cf
Daniel Hernandez <daniel@poliwin.mx>
2025-08-04 15:53:34 -0600

    Se agrega la estructura general para la validación, así como la versión y el prefijo.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        exposeUnsetFields: false, // Para no eliminar los campos no enviados
+      }
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
